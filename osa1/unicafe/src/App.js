2afc1dce0e0b63a8c765309d9e01f452f4ef27f6
Joni Joensuu <joni.joens@gmail.com>
2022-09-25 19:20:45 +0300

    osa1 tehtävät 1.6 - 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = (props) => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setGoodValue = () => {
+    setGood(good + 1)
+  }
+  const setNeutralValue = () => {
+    setNeutral(neutral + 1)
+  }
+  const setBadValue = () => {
+    setBad(bad + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={setGoodValue} text="good" />
+      <Button handleClick={setNeutralValue} text="neutral" />
+      <Button handleClick={setBadValue} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) => {
+  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+    return (
+      <div>
+      no feedback given
+      </div>
+    )
+  }
+  return (
+  <div>
+    <table>
+      <tbody>
+      <StatisticLine text="good" value={props.good} />
+    <StatisticLine text="neutral" value={props.neutral} />
+    <StatisticLine text="bad" value={props.bad} />
+    <StatisticLine text="all" value={props.good + props.neutral + props.bad} />
+    <StatisticLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad)} />
+    <StatisticLine text="positive" value={props.good / (props.good + props.neutral + props.bad) * 100} extraText=" %" />
+      </tbody>
+    </table>
+  </div>
+  )
+}
+
+const StatisticLine = props => 
+    <tr>
+        <td>
+          {props.text}
+        </td>
+        <td>
+          {props.value} {props.extraText}
+        </td>
+      </tr>
+
+
+
+export default App
