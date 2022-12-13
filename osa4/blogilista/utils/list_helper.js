290de7aa9d3e514b61b69f7b3802b9bb1b4fd2bd
Joni Joensuu <joni.joens@gmail.com>
2022-12-13 15:30:24 +0200

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -8,9 +8,27 @@ const totalLikes = (blogs) => {
     }, 0)
     return likes
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    } else if (blogs.length === 1) {
+        return blogs[0]
+    } else {
+        var favorite = blogs.reduce(function(max, blog) {
+            if (max.likes > blog.likes) {
+                return max
+            } else {
+                return blog
+            }
+        }, blogs[0])
+    }
+    return favorite
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
 
