0eb1b9f714e8fa7ae5a3f797e53de3cbfd9a6712
Joni Joensuu <joni.joens@gmail.com>
2022-12-31 10:31:24 +0200

    4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
