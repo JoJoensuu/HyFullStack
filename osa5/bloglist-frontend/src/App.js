f0c18c20ee1cb31544bbfdc1d2d91a512b9fd671
Joni Joensuu <joni.joens@gmail.com>
2023-01-08 14:10:43 +0200

    5.17

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -90,6 +90,7 @@ const App = () => {
             <div>
           username
                 <input
+                    id='username'
                     type="text"
                     value={username}
                     name="Username"
@@ -99,13 +100,14 @@ const App = () => {
             <div>
           password
                 <input
+                    id='password'
                     type="password"
                     value={password}
                     name="Password"
                     onChange={({ target }) => setPassword(target.value)}>
                 </input>
             </div>
-            <button type="submit">login</button>
+            <button id='login-button' type="submit">login</button>
         </form>
     )
 
