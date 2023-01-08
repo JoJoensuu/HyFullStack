7556212affafb7921987d35bdc4e988319c0b889
Joni Joensuu <joni.joens@gmail.com>
2023-01-08 10:26:02 +0200

    5.15

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -52,3 +52,31 @@ test('clicking button shows url, likes and user', async () => {
 
     expect(div).toHaveStyle('display: inline')
 })
+
+test('clicking like twice calls eventhandler twice', async () => {
+    const blog = {
+        title: 'Component testing is done with react-testing-library',
+        author: 'Janne Jannenen',
+        url: 'https://www.yle.fi',
+        likes: 0,
+        user: '123'
+    }
+    const user = {
+        username: 'testuser',
+        name: 'tester',
+        id: '123'
+    }
+
+    const mockHandler = jest.fn()
+
+    render(
+        <Blog blog={blog} user={user} addLike={mockHandler}/>
+    )
+
+    const testUser = userEvent.setup()
+    const button = screen.getByText('like')
+    await testUser.click(button)
+    await testUser.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+})
