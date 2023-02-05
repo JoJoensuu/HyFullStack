fcf38e206a9df6416e0beaa352ae54462499e869
Joni Joensuu <joni.joens@gmail.com>
2023-02-05 11:50:17 +0200

    8.5

--- osa8/index.js
+++ osa8/index.js
@@ -110,7 +110,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -119,7 +119,16 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => args.author ? books.filter(b => b.author === args.author) : books,
+    allBooks: (root, args) => {
+      returnedBooks = books
+      if (args.author) {
+        returnedBooks = returnedBooks.filter(b => b.author === args.author)
+      }
+      if (args.genre) {
+        returnedBooks = returnedBooks.filter(b => b.genres.includes(args.genre))
+      }
+      return returnedBooks
+    },
     allAuthors: () => authors
   },
   Author: {
