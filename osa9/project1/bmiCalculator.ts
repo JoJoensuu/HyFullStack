2a7c342de3d432a683489cab2fd27760ea912e1c
Joni Joensuu <joni.joens@gmail.com>
2023-02-12 16:21:34 +0200

    9.1

---
+++ osa9/project1/bmiCalculator.ts
@@ -0,0 +1,22 @@
+const calculateBmi = (height: number, weight: number): string => {
+    let meters = height / 100
+    let bmi = weight / (meters * meters)
+    if (bmi < 16) {
+        return 'Underweight (severe thinness)'
+    } else if (bmi < 16.9) {
+        return 'Underweight (Moderate thinness)'
+    } else if (bmi < 18.4) {
+        return 'Underweight (Mild thinness)'
+    } else if (bmi < 24.9) {
+        return 'Normal (healthy weight)'
+    } else if (bmi < 29.9) {
+        return 'Overweight (Pre-obese)'
+    } else if (bmi < 34.9) {
+        return 'Obese (Class I)'
+    } else if (bmi < 39.9) {
+        return 'Obese (Class II)'
+    }
+    return 'Obese (Class III)'
+}
+
+console.log(calculateBmi(180, 74))
