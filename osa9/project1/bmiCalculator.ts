273abb01a785479abe906f5b8d023a3c97d030b8
Joni Joensuu <joni.joens@gmail.com>
2023-02-16 15:47:14 +0200

    9.3

--- osa9/project1/bmiCalculator.ts
+++ osa9/project1/bmiCalculator.ts
@@ -2,21 +2,24 @@ const calculateBmi = (height: number, weight: number): string => {
     let meters = height / 100
     let bmi = weight / (meters * meters)
     if (bmi < 16) {
-        return 'Underweight (severe thinness)'
+        return 'Underweight (severe thinness)';
     } else if (bmi < 16.9) {
-        return 'Underweight (Moderate thinness)'
+        return 'Underweight (Moderate thinness)';
     } else if (bmi < 18.4) {
-        return 'Underweight (Mild thinness)'
+        return 'Underweight (Mild thinness)';
     } else if (bmi < 24.9) {
-        return 'Normal (healthy weight)'
+        return 'Normal (healthy weight)';
     } else if (bmi < 29.9) {
-        return 'Overweight (Pre-obese)'
+        return 'Overweight (Pre-obese)';
     } else if (bmi < 34.9) {
-        return 'Obese (Class I)'
+        return 'Obese (Class I)';
     } else if (bmi < 39.9) {
-        return 'Obese (Class II)'
+        return 'Obese (Class II)';
     }
-    return 'Obese (Class III)'
+    return 'Obese (Class III)';
 }
 
-console.log(calculateBmi(180, 74))
+const height: number = Number(process.argv[2]);
+const weight: number = Number(process.argv[3]);
+
+console.log(calculateBmi(height, weight));
--- osa9/project1/exerciseCalculator.ts
+++ osa9/project1/exerciseCalculator.ts
@@ -8,26 +8,45 @@ interface Result {
     average: number;
 }
 
+interface ParseHours {
+    testTarget: number;
+    testHours: number[]
+}
+
+const parseArguments = (args: string[]): ParseHours => {
+    const parsedArray = args.slice(3).map(h => parseFloat(h))
+    if (isNaN(Number(args[2]))) {
+        throw new Error('Provided values must be numbers!');
+    }
+    return {
+        testTarget: Number(args[2]),
+        testHours: parsedArray
+    }
+}
+
 const rating = (average: number, target: number): 1 | 2 | 3 => {
     if (average > target) {
-        return 3
+        return 3;
     } else if (target / average > 2) {
-        return 1
-    } return 2
+        return 1;
+    } return 2;
 }
 
 const ratingDescription = (rating: number): string => {
     if (rating === 1) {
-        return 'could be better'
+        return 'could be better';
     } else if (rating === 2) {
-        return 'not too bad but could be better'
-    } return 'excellent work'
+        return 'not too bad but could be better';
+    } return 'excellent work';
 }
 
-const calculateExercises = (hours: number[], target: number): Result => {
+const calculateExercises = (target: number, hours: number[]): Result => {
     let cumulativeHours = hours.reduce((cumulative, hours) => cumulative + hours, 0);
     let trainingDays = hours.reduce((cumulative, hours) => hours !== 0 ? cumulative + 1 : cumulative, 0);
     let average = cumulativeHours / hours.length;
+    if (isNaN(Number(average))) {
+        throw new Error('Provided values were not numbers!');
+    }
     return {
         periodLength: hours.length,
         trainingDays: trainingDays,
@@ -39,4 +58,13 @@ const calculateExercises = (hours: number[], target: number): Result => {
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+    const { testTarget, testHours } = parseArguments(process.argv);
+    console.log(calculateExercises(testTarget, testHours));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage)
+}
