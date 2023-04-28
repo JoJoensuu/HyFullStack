bf2d0293961c5f782ae3c9767ea23db5cfc5af13
Joni Joensuu <joni.joens@gmail.com>
2023-04-28 16:34:18 +0300

    9.9

--- osa9/project2/index.ts
+++ osa9/project2/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
+
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
@@ -12,3 +17,4 @@ app.get('/api/ping', (_req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
