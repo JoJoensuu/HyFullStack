7eafc1795f6ac8dc956227ebffb2e4e162be9359
Joni Joensuu <joni.joens@gmail.com>
2023-04-29 20:36:31 +0300

    9.12-9.13

---
+++ osa9/project2/src/services/patientService.ts
@@ -0,0 +1,36 @@
+import { v1 as uuid } from 'uuid';
+
+import patientData from '../../data/patients';
+
+import { PatientEntry, NonSensitivePatientEntry, NewPatientEntry } from '../types';
+
+const patients: PatientEntry[] = patientData;
+
+const getPatients = (): PatientEntry[] => {
+    return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatientEntry[] => {
+    return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+        id,
+        name,
+        dateOfBirth,
+        gender,
+        occupation,
+    }));
+};
+
+const addPatient = ( entry: NewPatientEntry ): PatientEntry => {
+    const newPatientEntry = {
+        id: uuid(),
+        ...entry
+    };
+    patients.push(newPatientEntry);
+    return newPatientEntry;
+};
+
+export default {
+    getPatients,
+    getNonSensitivePatients,
+    addPatient
+};
--- osa9/project2/src/types.ts
+++ osa9/project2/src/types.ts
@@ -1,5 +1,24 @@
+export interface PatientEntry {
+    id: string;
+    name: string;
+    dateOfBirth: string;
+    ssn: string;
+    gender: string;
+    occupation: string;
+}
+
 export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
 }
+
+export enum Gender {
+    Male = 'male',
+    Female = 'female',
+    Other = 'other',
+}
+
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
+
+export type NewPatientEntry = Omit<PatientEntry, 'id'>;
