6433740a04f82bf4299ac818f041f93db5b1c66e
Joni Joensuu <joni.joens@gmail.com>
2023-05-01 11:20:42 +0300

    9.15

--- osa9/part-d/src/App.tsx
+++ osa9/part-d/src/App.tsx
@@ -2,24 +2,104 @@ const App = () => {
 
   interface CourseName {
     name: string;
-  };
+  }
 
-  interface CoursePart {
+  interface CoursePartBase {
     name: string;
     exerciseCount: number;
+  }
+
+  interface CoursePartWithDescription extends CoursePartBase {
+    description: string;
+  }
+
+  interface CoursePartBasic extends CoursePartWithDescription {
+    kind: "basic"
+  }
+
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group"
+  }
+
+  interface CoursePartBackground extends CoursePartWithDescription {
+    backgroundMaterial: string;
+    kind: "background"
+  }
+
+  interface CoursePartRequirements extends CoursePartWithDescription {
+    requirements: string[];
+    kind: "special"
+  }
+
+  type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartRequirements;
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
   };
 
   const Header = (props: CourseName) => {
     return <h1>{props.name}</h1>
   };
 
+  const Part = ({ part }: { part: CoursePart }) => {
+      switch (part.kind) {
+        case "basic":
+          return (
+            <div>
+              <p>
+                <b>{part.name} {part.exerciseCount}</b>
+                <br></br>
+                {part.description}
+              </p>
+            </div>
+          );
+        case "group":
+          return (
+            <div>
+              <p>
+                <b>{part.name} {part.exerciseCount}</b>
+                <br></br>
+                project exercises: {part.groupProjectCount}
+              </p>
+            </div>
+          );
+        case "background":
+          return (
+            <div>
+              <p>
+                <b>{part.name} {part.exerciseCount}</b>
+                <br></br>
+                {part.description}
+                <br></br>
+                background material: {part.backgroundMaterial}
+              </p>
+            </div>
+          );
+        case "special":
+          return (
+            <div>
+              <p>
+                <b>{part.name} {part.exerciseCount}</b>
+                <br></br>
+                {part.description}
+                <br></br>
+                required skills: {part.requirements}
+              </p>
+            </div>
+          )
+      default:
+        return assertNever(part);
+      }
+  };
+
   const Content = ({ parts }: { parts: CoursePart[] }) => {
     return (
       <div>
-        {parts.map(({ name, exerciseCount }) => (
-          <p key={name}>
-             {name} {exerciseCount}
-          </p>
+        {parts.map((part, index) => (
+          <Part key={index} part={part} />
         ))}
       </div>
     );
@@ -31,19 +111,45 @@ const App = () => {
   };
 
   const courseName = "Half Stack application development";
-  const courseParts: { name: string; exerciseCount: number }[] = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
-    }
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
+    },
   ];
 
   return (
