0cd80ca9fde6ffb4543916e01cfd6395bf8b6aba
Joni Joensuu <joni.joens@gmail.com>
2023-05-02 23:38:43 +0300

    9.17

--- osa9/flight-diary-backend/frontend/flightdiariesfront/src/App.tsx
+++ osa9/flight-diary-backend/frontend/flightdiariesfront/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { DiaryEntry } from './types';
+import { DiaryEntry, Weather, Visibility, NewDiaryEntry } from './types';
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  const [newComment, setNewComment] = useState('');
 
   useEffect(() => {
     axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries').then(response => {
@@ -11,8 +15,50 @@ const App = () => {
     })
   }, [])
 
+  const diaryCreation = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const diaryToAdd: NewDiaryEntry = {
+      date: newDate,
+      weather: newWeather as Weather,
+      visibility: newVisibility as Visibility,
+      comment: newComment
+    };
+    try {
+      const response = await axios.post<DiaryEntry>('http://localhost:3000/api/diaries', diaryToAdd);
+      setDiaries(diaries.concat(response.data));
+    } catch(error) {
+      console.error('Error creating diary entry:', error);
+    }
+
+    setNewDate('')
+    setNewWeather('')
+    setNewVisibility('')
+    setNewComment('')
+  };
+
   return (
     <div>
+      <h1>Add new entry</h1>
+      <form onSubmit={diaryCreation}>
+        date<input
+          value={newDate}
+          onChange={(event) => setNewDate(event.target.value)}
+        /><br></br>
+        weather<input
+          value={newWeather}
+          onChange={(event) => setNewWeather(event.target.value)}
+        /><br></br>
+        visibility<input
+          value={newVisibility}
+          onChange={(event) => setNewVisibility(event.target.value)}
+        /><br></br>
+        comment<input
+          value={newComment}
+          onChange={(event) => setNewComment(event.target.value)}
+        /><br></br>
+        <br></br>
+        <button type='submit'>Add</button>
+      </form>
       <h1>Diary entries</h1>
         {diaries.map(diary =>
           <div key={diary.id}>
