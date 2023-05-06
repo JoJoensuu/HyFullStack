834c9114ac88296917efba95dd9f7f4d87948314
Joni Joensuu <joni.joens@gmail.com>
2023-05-06 20:00:10 +0300

    9.22

--- osa9/project2/src/types.ts
+++ osa9/project2/src/types.ts
@@ -1,6 +1,31 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
 
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: {
+        startDate: string;
+        endDate: string;
+    };
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: {
+        date: string;
+        criteria: string;
+    };
 }
 
 export interface Patient {
@@ -24,6 +49,23 @@ export enum Gender {
     Female = 'female',
     Other = 'other',
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthCareEntry
+    | HealthCheckEntry;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn'>;
 
