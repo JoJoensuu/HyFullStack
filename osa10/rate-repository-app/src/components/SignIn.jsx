dfb48fb850044db332140c24046e3dfa1907e33f
Joni Joensuu <joni.joens@gmail.com>
2024-03-23 17:49:05 +0200

    10.9

--- osa10/rate-repository-app/src/components/SignIn.jsx
+++ osa10/rate-repository-app/src/components/SignIn.jsx
@@ -1,72 +1,63 @@
-import { View, TextInput, StyleSheet, Pressable, Text } from 'react-native';
-import { useFormik } from 'formik';
+import { StyleSheet, View, Button } from 'react-native';
+import { Formik } from 'formik';
+import FormikTextInput from './TextInput';
+import * as yup from 'yup';
+
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      paddingTop: 20,
-      backgroundColor: '#FFF',
-    },
-    input: {
-      width: '80%',
-      height: 40,
-      borderWidth: 1,
-      borderColor: 'gray',
-      borderRadius: 5,
-      paddingLeft: 10,
-      marginTop: 10,
-    },
-    button: {
-      backgroundColor: '#0366d6',
-      width: '80%',
-      height: 40,
-      borderRadius: 5,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 10,
-    },
-    buttonText: {
-        color: '#FFFFFF',
-        fontWeight: 'bold',
-      }
-  });
+  container: { 
+    padding: 20
+  },
+  submit: {
+    marginTop: 20,
+  }
+});
 
-const SignIn = () => {
-  const initialValues = {
-        username: '',
-        password: '',
-  };
-
-  const onSubmit = (values) => {
-        console.log(values);
-  };
-
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-  })
-
-  return (
-    <View style={styles.container}>
-        <TextInput
-            style={styles.input}
-            placeholder="Username"
-            value={formik.values.username}
-            onChangeText={formik.handleChange('username')}
-        />
-        <TextInput
-            style={styles.input}
-            placeholder="Password"
-            value={formik.values.password}
-            secureTextEntry
-            onChangeText={formik.handleChange('password')}
-        />
-        <Pressable style={styles.button} onPress={formik.handleSubmit}>
-            <Text style={styles.buttonText}>Sign in</Text>
-        </Pressable>
-    </View>
-    );
+const initialValues = {
+  username: '',
+  password: '',
 };
 
-export default SignIn;
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(4, 'Username must be at least 4 characters')
+    .required('Username is required'),
+  password: yup
+    .string()
+    .required('Please Enter your password')
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Must Contain 8 Characters: One Uppercase, One Lowercase, One Number and one special case Character"
+    )
+});
+
+const LoginForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.container}>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" secureTextEntry={true} />
+      <View style={styles.submit}>
+        <Button onPress={onSubmit} title="Sign in"/>
+      </View>
+    </View>
+  );
+};
+
+const SignIn = () => {
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik 
+      initialValues={initialValues} 
+      onSubmit={onSubmit}       
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => <LoginForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
+};
+
+export default SignIn
