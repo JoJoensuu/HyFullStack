e7e26a8a2f9c8ba9353e16c92ddc05a76671a69f
Joni Joensuu <joni.joens@gmail.com>
2024-05-06 14:40:25 +0300

    10.24

--- osa10/rate-repository-app/src/components/RepositoryList.jsx
+++ osa10/rate-repository-app/src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { FlatList, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, TextInput } from 'react-native';
+import { useDebounce } from "use-debounce";
 import { RepositoryItem } from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import Sorter from './Sorter';
@@ -9,6 +10,12 @@ const styles = StyleSheet.create({
     height: 10,
     backgroundColor: '#e1e4e8'
   },
+  listHeaderComponent: {
+    backgroundColor: '#e1e4e8',
+  },
+  input: {
+    backgroundColor: '#ffffff'
+  }
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
@@ -16,27 +23,39 @@ const ItemSeparator = () => <View style={styles.separator} />;
 const RepositoryList = () => {
   const [orderBy, setOrderBy] = useState("CREATED_AT" || "RATING_AVERAGE");
   const [orderDirection, setOrderDirection] = useState("DESC" || "ASC");
-  const { repositories } = useRepositories(orderBy, orderDirection);
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [debouncedSearchKeyword] = useDebounce(searchKeyword, 100);
+  const { repositories } = useRepositories(orderBy, orderDirection, debouncedSearchKeyword);
 
   const repositoryNodes = repositories && repositories.edges ? repositories.edges.map(edge => edge.node) : [];
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => (
-            <RepositoryItem item={item}/>
-      )}
-      keyExtractor={(item) => item.id}
-      ListHeaderComponent={() => (
+    <>
+      <View style={styles.listHeaderComponent}>
+        <TextInput
+          style={styles.input}
+          inlineImageLeft='search_icon'
+          placeholder='Search'
+          value={searchKeyword}
+          onChangeText={(text) => setSearchKeyword(text)}
+        />
         <Sorter
           orderBy={orderBy}
           setOrderBy={setOrderBy}
           orderDirection={orderDirection}
           setOrderDirection={setOrderDirection}
         />
-      )}
-    />
+      </View>
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => (
+            <RepositoryItem item={item}/>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </>
+    
   );
 };
 
